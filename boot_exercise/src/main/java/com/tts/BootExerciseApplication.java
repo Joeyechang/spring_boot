c7b324a7f59452923873923ec43fa43f7126b702
leongfeng <leongfeng@163.com>
2016-10-27 23:26:24 +0800

    add sub modules

---
+++ boot_exercise/src/main/java/com/tts/BootExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.tts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootExerciseApplication.class, args);
+	}
+}
