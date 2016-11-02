201e61328b33c307b4731d0a559bb5b5877fdba8
mike.leong <mike.leong@easternphoenix.com>
2016-11-02 21:39:42 +0800

    add mockito module

---
+++ junit_mockito/src/main/java/com/tts/JunitMockitoApplication.java
@@ -0,0 +1,12 @@
+package com.tts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitMockitoApplication.class, args);
+	}
+}
