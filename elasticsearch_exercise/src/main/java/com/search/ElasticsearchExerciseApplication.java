0932a75bdd94d1161520b811622bd6f966ec3226
mike.leong <mike.leong@easternphoenix.com>
2016-11-29 21:23:36 +0800

    elasticsearch

---
+++ elasticsearch_exercise/src/main/java/com/search/ElasticsearchExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchExerciseApplication.class, args);
+	}
+}
